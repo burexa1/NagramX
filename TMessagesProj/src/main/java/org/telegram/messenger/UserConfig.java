b271659442e87edf07be800c3c5387c54aa5073b
burexa1 <54174747+burexa1@users.noreply.github.com>
2026-06-25 16:19:17 +0300

    Update UserConfig.java

--- TMessagesProj/src/main/java/org/telegram/messenger/UserConfig.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/UserConfig.java
@@ -26,8 +26,8 @@ import tw.nekomimi.nekogram.NekoConfig;
 public class UserConfig extends BaseController {
 
     public static int selectedAccount;
-    public final static int MAX_ACCOUNT_DEFAULT_COUNT = 10;
-    public final static int MAX_ACCOUNT_COUNT = 10;
+    public final static int MAX_ACCOUNT_DEFAULT_COUNT = 35;
+    public final static int MAX_ACCOUNT_COUNT = 35;
 
     private final Object sync = new Object();
     private volatile boolean configLoaded;
